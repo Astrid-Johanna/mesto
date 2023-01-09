6f84aaf5d13de35c3f4289c199a1b55c936a4b69
Astrid Pavlova <astrid.pavlova@yandex.ru>
2023-01-09 23:03:18 +0300

    сделать базовую верстку сделать header и profile

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,29 @@ let editButton = document.querySelector('.profile__editbutton');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
 
-// function openPopup() {
-//     popup.classList.add('popup_opened');
-// }
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__nameinput');
+let jobInput = document.querySelector('.form__jopinput');
 
-// editButton.addEventListener('click', openPopup);
-
-// function closePopup() {
-//     popup.classList.remove('popup_opened');
-// }
-
-// popupClose.addEventListener('click', closePopup);
-
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
+function openPopup() {
+    popup.classList.add('popup_opened');
 }
 
-editButton.addEventListener('click', togglePopup);
-popupClose.addEventListener('click', togglePopup);
+editButton.addEventListener('click', openPopup);
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+popupClose.addEventListener('click', closePopup);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
