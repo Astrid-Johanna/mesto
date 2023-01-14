054f5d3a0b4252d3984ab093154dc08776ddeef8
Astrid Pavlova <astrid.pavlova@yandex.ru>
2023-01-14 22:37:33 +0300

    js

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,7 @@ let editButton = document.querySelector('.profile__editbutton');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
 
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__nameinput');
-let jobInput = document.querySelector('.form__jopinput');
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -18,13 +16,29 @@ function closePopup() {
 
 popupClose.addEventListener('click', closePopup);
 
+// Находим форму в DOM
+let formElement = document.querySelector('.form');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__form-input-name');
+let jobInput = document.querySelector('.popup__form-input-jop');
+
+
 function handleFormSubmit (evt) {
     evt.preventDefault();
+
     // Получите значение полей jobInput и nameInput из свойства value
+    nameInput.value;
+    jobInput.value;
 
     // Выберите элементы, куда должны быть вставлены значения полей
+    let name = document.querySelector('.profile__name');
+    let job = document.querySelector('.profile__job');
 
     // Вставьте новые значения с помощью textContent
+    name.textContent = "";
+    job.textContent =  "";
+    //вызываем функцию чтобы форма закрылвсь после нажатия сохранить.
+    closePopup()
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
