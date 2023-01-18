8b2aa1fecb482b53cd4728b325d5195ff18749d9
Astrid Pavlova <astrid.pavlova@yandex.ru>
2023-01-18 22:25:07 +0300

    праки после 1 ревью

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,11 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupClose = document.querySelector('.popup__close');
 
-
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('jop');
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -14,31 +18,24 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-popupClose.addEventListener('click', closePopup);
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__form-input-name');
-let jobInput = document.querySelector('.popup__form-input-jop');
-
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
 
     // Получите значение полей jobInput и nameInput из свойства value
-    const Name = nameInput.value;
-    const Job = jobInput.value;
+    const name = nameInput.value;
+    const job = jobInput.value;
 
     // Выберите элементы, куда должны быть вставлены значения полей
     const profileName = document.querySelector('.profile__name');
     const profilejob = document.querySelector('.profile__job');
 
     // Вставьте новые значения с помощью textContent
-    profileName.textContent = Name;
-    profilejob.textContent =  Job;
+    profileName.textContent = name;
+    profilejob.textContent =  job;
     //вызываем функцию чтобы форма закрылвсь после нажатия сохранить.
     closePopup()
 }
+
+popupClose.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', handleFormSubmit);
